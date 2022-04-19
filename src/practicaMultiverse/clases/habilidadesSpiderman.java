af7b0dae35786571a3b73bafad9e3e587f950df8
Romario117 <rfls_93@hotmail.com>
2022-04-19 13:33:35 -0500

    v 1.0 Interfaces de habilidades

---
+++ src/practicaMultiverse/clases/habilidadesSpiderman.java
@@ -0,0 +1,15 @@
+package practicaMultiverse.clases;
+
+public interface habilidadesSpiderman {
+
+    void sentidoAracnido(int experiencia);
+
+    int golpeDebil(int fuerza, int resistencia);
+
+    int golpeFuerte(int fuerza, int resistencia);
+
+    int patadaDebil(int fuerza, int resistencia);
+
+    int patadaFuerte(int fuerza, int resistencia);
+
+}
