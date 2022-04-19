ae2396037ac587c3868847fe8cb669c4c6173474
Romario117 <rfls_93@hotmail.com>
2022-04-19 10:09:43 -0500

    Agregado de 5 modelos mas de los personajes requeridos

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,20 @@ import practicaHarry.PersonajesHarry;
 
 public class Main {
     public static void main(String[] args) {
-        PersonajesHarry pH = new PersonajesHarry("Newton \"Newt\" Artemis Fido Scamander","Hufflepuff","escritorio de oficina","Kelpie");
+        PersonajesHarry newt = new PersonajesHarry("Newton \"Newt\" Artemis Fido Scamander","Hufflepuff","escritorio de oficina","Kelpie");
+        PersonajesHarry potter = new PersonajesHarry("Harry Potter","Gryffindor","Dementor","Stag");
+        PersonajesHarry goyle = new PersonajesHarry("Gregory Goyle", "Slytherin","Lord Voldemort", "None");
+        PersonajesHarry voldemort = new PersonajesHarry("Tom Marvolo Riddle", "Slytherin", "Muerte","None");
+        PersonajesHarry dumbledore = new PersonajesHarry("Albus Percival Wulfric Brian Dumbledore","Gryffindor"," Muerte de su hermana", "Fenix");
 
-        System.out.println("Nombre : " +pH.getNombre() + " \n" + "Casa :" + pH.getCasa() + " \n Boggart:" + pH.getBoggart() + " \n Patronus :" + pH.getPatronus());
+
+
+        System.out.println("Nombre : " +newt.getNombre() + " \n" + "Casa :" + newt.getCasa() + " \n Boggart:" + newt.getBoggart() + " \n Patronus :" + newt.getPatronus());
+        System.out.println("Nombre : " +potter.getNombre() + " \n" + "Casa :" + potter.getCasa() + " \n Boggart:" + potter.getBoggart() + " \n Patronus :" + potter.getPatronus());
+        System.out.println("Nombre : " +goyle.getNombre() + " \n" + "Casa :" + goyle.getCasa() + " \n Boggart:" + goyle.getBoggart() + " \n Patronus :" + goyle.getPatronus());
+        System.out.println("Nombre : " +voldemort.getNombre() + " \n" + "Casa :" + voldemort.getCasa() + " \n Boggart:" + voldemort.getBoggart() + " \n Patronus :" + voldemort.getPatronus());
+        System.out.println("Nombre : " +dumbledore.getNombre() + " \n" + "Casa :" + dumbledore.getCasa() + " \n Boggart:" + dumbledore.getBoggart() + " \n Patronus :" + dumbledore.getPatronus());
+
 
     }
 }
